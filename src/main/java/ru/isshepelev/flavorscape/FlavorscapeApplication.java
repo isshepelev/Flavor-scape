e685d4c22ceb814d9c5bd067d29c60c99c3415a3
Ilya Shepelev <isshepelev2004@gmail.com>
2025-05-25 14:54:46 +0300

    first commit

---
+++ src/main/java/ru/isshepelev/flavorscape/FlavorscapeApplication.java
@@ -0,0 +1,13 @@
+package ru.isshepelev.flavorscape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlavorscapeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlavorscapeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/isshepelev/flavorscape/FlavorscapeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.isshepelev.flavorscape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class FlavorscapeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
